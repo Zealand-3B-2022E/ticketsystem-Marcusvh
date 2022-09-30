c831829a01454e0e4e87298ea6f9ff5bd86116e2
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-09-30 15:45:46 +0200

    created the Test project

---
+++ TicketSystemClassLibraryTests/CarTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketSystemClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
