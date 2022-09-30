244f409d8cb0ea65a2658151a643e15e020f6162
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-09-30 15:45:24 +0200

    First step. created Car class with content

---
+++ TicketSystemClassLibrary/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace TicketSystemClassLibrary
+{
+    public class Car
+    {
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+
+        /// <summary>
+        /// Price for a car to travel through
+        /// </summary>
+        /// <returns>Price in DKK</returns>
+        public double Price()
+        {
+            return 240;
+        } //The price is fixed at 240 which must be returned
+
+        /// <summary>
+        /// Specifies What kind of vehicle.
+        /// </summary>
+        /// <returns>The vehicle type as a string</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }//which returns the string "Car"
+
+
+    }
+}
