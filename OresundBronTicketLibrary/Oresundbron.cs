906c951c71071e8997024255b53a98abe75720f4
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-10-07 20:25:55 +0200

    added Oresundbron with eksternt dll

---
+++ OresundBronTicketLibrary/Oresundbron.cs
@@ -0,0 +1,16 @@
+﻿using TicketSystemClassLibrary;
+
+namespace OresundBronTicketLibrary
+{
+    public class Oresundbron: Vehicle
+    {
+        //public override double Price(bool broBizz)
+        //{
+            
+        //}
+        //public override string VehicleType()
+        //{
+        //    return $"welp";
+        //}
+    }
+}
