a66c41b8e4b710ccb7372a9b5fbbdd051de9209f
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-10-07 17:51:57 +0200

    added new library. modifired Price method in car so it supports weekend discount

--- TicketSystemClassLibrary/Car.cs
+++ TicketSystemClassLibrary/Car.cs
@@ -5,13 +5,20 @@
     /// </summary>
     public class Car: Vehicle
     {
+        public int BrobizzDiscountAmount { get; set; } = 5;
+        public int WeekendDiscountAmount { get; set; } = 20;
+
         /// <summary>
-        /// Price for a car to travel through with or without BroBizz
+        /// Price for a car to travel through with or without BroBizz and weekend discount
         /// </summary>
         /// <returns>Price in DKK</returns>
         public double Price(bool broBizz)
         {
-            return Vehicle.Price(240, broBizz);
+            double price = 240;
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+                price = 240 * (1 - (WeekendDiscountAmount / 100.0));
+
+            return broBizz ? price * (1 - (BrobizzDiscountAmount / 100.0)) : 240;
         } 
         /// <summary>
         /// Price for a car to travel through
